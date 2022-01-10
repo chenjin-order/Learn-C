e4df858480f5a444a4d4455db2f8f4ff7ab6c7f4
chenjin-order <305927199@qq.com>
2022-01-10 22:20:22 +0800

    third commit

--- example.c
+++ example.c
@@ -1,9 +1,17 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-#include <string.h>
+int Add(int x, int y)
+{
+	int z = 0;
+	z = x + y;
+	return z;
+}
 int main()
 {
-	printf("%d\n", strlen("abcdef"));
-	printf("%d\n", strlen("c:\test\328\test.c"));
+	int sum1 = 0;
+	int sum2 = 0;
+	scanf("%d%d", &sum1, &sum2);
+	int sum = Add(sum1, sum2);
+	printf("%d\n", sum);
 	return 0;
 }
