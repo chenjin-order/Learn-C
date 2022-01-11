80324f3f22cf3189433c8cf2cbc2b6ab20867a47
chenjin-order <305927199@qq.com>
2022-01-12 00:20:57 +0800

    fourth commit

--- example.c
+++ example.c
@@ -1,17 +1,10 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-int Add(int x, int y)
-{
-	int z = 0;
-	z = x + y;
-	return z;
-}
 int main()
 {
-	int sum1 = 0;
-	int sum2 = 0;
-	scanf("%d%d", &sum1, &sum2);
-	int sum = Add(sum1, sum2);
-	printf("%d\n", sum);
+	int a=1;
+	int b = (a++) + (a++) + (a++);
+	printf("%d\n", a);
+	printf("%d\n", b);
 	return 0;
 }
