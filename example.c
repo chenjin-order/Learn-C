00c92e197a3ca831d4fa7c81ead60cb56f3dfba8
chenjin-order <305927199@qq.com>
2022-01-12 23:49:51 +0800

    constant variables

--- example.c
+++ example.c
@@ -1,12 +1,28 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
+typedef unsigned int u_int;
+//extern int g_val;
+//void test()
+//{
+//	static int a = 1;
+//	a++;
+//	printf("%d ", a);
+//}
+extern int Add(int, int); 
 int main()
 {
-	int a=1;
-	int b = (a++) + (a++) + (a++);
-	printf("%d\n", a);
-	printf("%d\n", b);
-	int arr[10] = { 0 };
-	printf("%d\n", arr[0]);
+	/*unsigned int num = 100;
+	u_int num1 = 320;
+	int i = 0;
+	while (i<10)
+	{
+		test();
+		i++;
+	}*/
+	/*printf("%d\n", g_val);*/
+	int a = 10;
+	int b = 20;
+	int sum = Add(a, b);
+	printf("sum = %d\n", sum);
 	return 0;
 }
