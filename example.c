3b399eed327a6a71821ab0c31dd90b0ac179c7e1
chenjin-order <305927199@qq.com>
2022-01-14 22:17:48 +0800

    branch statement

--- example.c
+++ example.c
@@ -1,28 +1,15 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h> 
-struct Stu
-{
-	char name[20];
-	int age;
-	double score;
-};
-struct Book
-{
-	char name[20];
-	float price;
-	char id[30];
-};
 int main()
 {
-	struct Stu s = { "张三",20,85.5 };
-
-	printf("1:%s %d %lf\n", s.name, s.age, s.score);
-
-	struct Stu* ps = &s;
-
-	printf("2:%s %d %lf\n", (*ps).name, (*ps).age, (*ps).score);
-
-	printf("3:%s %d %lf\n", ps->name, ps->age, ps->score);
-	
+	int age = 20;
+	if (age < 18)
+		printf("少年");
+	else if (age >= 18 && age < 40)
+		printf("青年");
+	else if (age >= 40 && age < 60)
+		printf("中年");
+	else
+		printf("晚年");
 	return 0;
 }
