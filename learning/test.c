465f33dab4f59b5df08fe16aa78ea18632ff72bc
chenjin-order <305927199@qq.com>
2022-02-10 16:20:25 +0800

    learning.

--- learning/test.c
+++ learning/test.c
@@ -1,12 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
 int main()
 {
-	int i = 0;
-	for (i = 1; i <= 10; i++)
+	int a = 0;
+	int n = 0;
+	scanf("%d", &n);
+	for (int i = 1 ;i <= n ;i++)
 	{
-		if (i == 5)
-			continue;
-		printf("%d ", i);
+		int m = 1;
+		for (int j = 1;j <= i ;j++)
+		{
+			m *= j;
+		}
+		a += m;
 	}
+	printf("%d\n", a);
 	return 0;
 }
