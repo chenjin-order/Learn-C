a64799811ecde87fd2b0180b3cd50c2374a8f8fd
chenjin-order <305927199@qq.com>
2022-02-27 23:42:32 +0800

    study

--- learning/learn.c
+++ learning/learn.c
@@ -3,28 +3,68 @@
 #include <stdio.h>
 #include <string.h>
 #include <Windows.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main()
+void menu()
 {
-	int i = 0;
-	char password[20] = { 0 };
-	for ( i = 0; i < 3; i++)
+	printf("************************************\n");
+	printf("************   1.play   ************\n");
+	printf("************   0.exit   ************\n");
+	printf("************************************\n");
+}
+
+void game()
+{
+	//猜数字实现
+	//1.生成随机数
+	
+	int ret = rand() % 100 + 1;
+	//2.猜数字
+	int guess = 0;
+
+	while (1)
 	{
-		printf("请输入密码:>");
-		scanf("%s", password);
-		if (strcmp(password,"123456") == 0)
+		printf("请猜数字:>");
+		scanf("%d", &guess);
+		if (guess < ret)
 		{
-			printf("登录成功\n");
-			break;
+			printf("猜小了\n");
+		}
+		else if (guess > ret)
+		{
+			printf("猜大了\n");
 		}
 		else
 		{
-			printf("密码错误,请重新输入!\n");
+			printf("恭喜你，猜对了\n");
+			break;
 		}
 	}
-	if (i == 3)
+}
+int main()
+{
+	int input = 0;
+
+	srand((unsigned int)time(NULL));
+
+	do
 	{
-		printf("密码输入错误，退出程序");
-	}
+		menu();//打印菜单
+		printf("请选择:>");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			game();
+			break;
+		case 0:
+			printf("退出游戏\n");
+			break;
+		default:
+			printf("选择错误，请重新选择\n");
+			break;
+		}
+	} while (input);
 	return 0;
 }
