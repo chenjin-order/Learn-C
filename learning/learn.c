aa7b1f4ececbb27caccc7f96b7d7b5d33c45c8ea
chenjin-order <305927199@qq.com>
2022-03-01 00:03:00 +0800

    study

--- learning/learn.c
+++ learning/learn.c
@@ -1,70 +1,31 @@
 #define _CRT_SECURE_NO_WARNINGS 
 
 #include <stdio.h>
-#include <string.h>
-#include <Windows.h>
-#include <stdlib.h>
-#include <time.h>
 
-void menu()
-{
-	printf("************************************\n");
-	printf("************   1.play   ************\n");
-	printf("************   0.exit   ************\n");
-	printf("************************************\n");
-}
-
-void game()
-{
-	//猜数字实现
-	//1.生成随机数
-	
-	int ret = rand() % 100 + 1;
-	//2.猜数字
-	int guess = 0;
-
-	while (1)
-	{
-		printf("请猜数字:>");
-		scanf("%d", &guess);
-		if (guess < ret)
-		{
-			printf("猜小了\n");
-		}
-		else if (guess > ret)
-		{
-			printf("猜大了\n");
-		}
-		else
-		{
-			printf("恭喜你，猜对了\n");
-			break;
-		}
-	}
-}
 int main()
 {
-	int input = 0;
-
-	srand((unsigned int)time(NULL));
-
-	do
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	scanf("%d%d%d", &a, &b, &c);
+	if (a < b)
 	{
-		menu();//打印菜单
-		printf("请选择:>");
-		scanf("%d", &input);
-		switch (input)
-		{
-		case 1:
-			game();
-			break;
-		case 0:
-			printf("退出游戏\n");
-			break;
-		default:
-			printf("选择错误，请重新选择\n");
-			break;
-		}
-	} while (input);
+		int tmp = a;
+		a = b;
+		b = tmp;
+	}
+	if (a < c)
+	{
+		int tmp = a;
+		a = c;
+		c = tmp;
+	}
+	if (b < c)
+	{
+		int tmp = b;
+		b = c;
+		c = tmp;
+	}
+	printf("%d %d %d\n", a, b, c);
 	return 0;
 }
