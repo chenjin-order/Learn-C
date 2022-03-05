aef06a93e5aee878e6f2ed856b121c588a24dcb8
chenjin-order <305927199@qq.com>
2022-03-05 16:53:21 +0800

    study

--- learning/learn.c
+++ learning/learn.c
@@ -1,42 +1,23 @@
 #define _CRT_SECURE_NO_WARNINGS 
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main()
 {
-	int m = 0;
-	int n = 0;
-	
-	scanf("%d%d", &m, &n);
-	
-	/*int max = 0;
-
-	if (m > n)
+	char input[20] = { 0 };
+	system("shutdown -s -t 60 ");
+again:	
+	printf("请注意，你的电脑将在一分钟之后关机，请输入取消关机，就停止关机\n");
+	scanf("%s", input);
+	if (strcmp(input, "取消关机") == 0)
 	{
-		max = n;
+		system("shutdown -a");
 	}
 	else
 	{
-		max = m;
+		goto again;
 	}
-
-	while (1)
-	{
-		if (m %  max == 0 && n % max == 0)
-		{
-			printf("最大公约数是： %d\n", max);
-			break;
-		}
-		max--;
-	}*/
-	int t = 0;
-
-	while (t = m % n)
-	{
-		m = n; 
-		n = t;
-	}
-
-	printf("最大公约数是： %d\n", n);
 	return 0;
 }
